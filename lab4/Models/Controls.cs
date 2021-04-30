921fb7a4a73dcd0bffa0458f842bebf97395f4cf
Ilya T <tulyavkoilya23@gmail.com>
2021-05-01 00:34:48 +0700

    lab4 done

---
+++ lab4/Models/Controls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class Controls
+    {
+        public string TextBox { get; set; } = null;
+        public string TextArea { get; set; } = null;
+        public string Radio { get; set; } = null;
+        public string DropDownList { get; set; } = null;
+        public string ListBox { get; set; } = null;
+        public string Result { get; set; }
+        public string ControlElement { get; set; }
+        public string Name { get; set; }
+
+        public Controls()
+        {
+
+        }
+    }
+}
